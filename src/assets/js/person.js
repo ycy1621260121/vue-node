f649e7bbd3591c972e0a35263d5cae364e83d551
杨超越工作室 <1621260121@qq.com>
2019-12-02 14:57:02 +0800

    Update person.js

--- src/assets/js/person.js
+++ src/assets/js/person.js
@@ -24,4 +24,27 @@ class Person {
   //END
 });*/
 
+
+//闭包
+/*function showHelp(help) {
+  document.getElementById('help').innerHTML = help;
+}
+
+function setupHelp() {
+  var helpText = [
+      {'id': 'email', 'help': 'Your e-mail address'},
+      {'id': 'name', 'help': 'Your full name'},
+      {'id': 'age', 'help': 'Your age (you must be over 16)'}
+    ];
+
+  for (var i = 0; i < helpText.length; i++) {
+    let item = helpText[i];
+    document.getElementById(item.id).onfocus = function() {
+      showHelp(item.help);
+    }
+  }
+}
+
+setupHelp();*/
+
 export default Person
